7758e335728ebf7ec66174a7f162ab24765a8601
Bongani Sumbane <Bonganezitha07@gmail.com>
2026-09-24 23:04:14 +0000

    Added product id to assign prices using js and aslo changed the syling for media devices with screen of 700 to lower and also added products in the js file of prices and added changed a few styles

---
+++ scripts/prices.js
@@ -0,0 +1,35 @@
+"use strict"
+const prices=document.querySelector("#priceCard");
+
+const bakedGoodsPrices=[
+    {name:"Chocolate croissants",
+    price:35,
+    productNo:1,
+    available:true},
+    
+    {name:"Banana bread",
+    price:45,
+    productNo:2,
+    available:true},
+
+    {name:"Blueberry muffins",
+    price:25,
+    productNo:3,
+    available:true},
+    
+    {name:"Cookies",
+    price:15,
+    productNo:4,
+    available:true},
+
+    {name:"Cinnamon rolls",
+    price:30,
+    productNo:5,
+    available:true}
+
+]
+
+// function to match the products and match with the prices
+function mathcingProductToPrice(){
+  
+}
